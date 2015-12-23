81ebfeadeeaa10eedbff55c1173404b8b44ca0c9
stefaan himpe <stefaan.himpe@gmail.com>
2015-12-23 21:55:50 +0100

    add a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='bluegrass',
+        version='0.0.1',
+        packages=[''],
+        url='https://github.com/shimpe/bluegrass',
+        license='GPL3',
+        author='stefaan himpe',
+        author_email='stefaan.himpe@gmail.com',
+        description='music style compiler'
+)
